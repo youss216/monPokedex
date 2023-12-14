c1b5ef3290524116fc6e4fa58cbb66ee4306e278
youssef <youssefamami02@gmail.com>
2023-12-14 15:44:31 +0100

    ajout de l'affichage dynamique de pokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,31 @@
 function PokemonCard() {
-
+   const pokemonList = [
+      {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+      const pokemon = pokemonList[1]
    return <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-      <figcaption>Bulbasaur</figcaption>
+
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="" />: <p>???</p>}
+      
+      {console.log(pokemon.imgSrc)}
+      <figcaption>{pokemon.name}</figcaption>
    </figure>
+
+
+
 }
 
 export default PokemonCard;
+
+
+
 
 
 
