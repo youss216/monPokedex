614b463bc45e1cf428b24c2726e05bff3e2c7c8e
youssef <youssefamami02@gmail.com>
2023-12-18 16:50:08 +0100

    prop ajouté dans pokemonCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    
+    },
+  ];
+    
+  
+
+   const pokemon = pokemonList[0]
+
   return ( 
-    <div>
-      <PokemonCard />
+    <div className="pokemon-Card">
+      
+     <PokemonCard pokemon= {pokemon}/>  
+      
+      
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,8 @@
-function PokemonCard() {
-   const pokemonList = [
-      {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
-      const pokemon = pokemonList[1]
+function PokemonCard({pokemon}) {
+
+   console.log(pokemon);
+   
+      // const pokemon = pokemonList[1]
    return <figure>
 
       {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="" />: <p>???</p>}
